48c7e6d4c8c4504af635936614054a2352b84a31
OseungKwon <00osos@naver.com>
2025-06-27 22:52:36 +0900

    refactor: 사용 안하는 api 파일 제거

--- shared/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const apiClient = axios.create({
-	// TODO: 환경변수로 수정
-	baseURL: 'http://localhost:3001',
-	headers: {
-		'Content-Type': 'application/json',
-	},
-});
