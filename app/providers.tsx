1c8219a5cdb92de51c27bc34bd7f8e6a09e81f59
OseungKwon <00osos@naver.com>
2025-06-27 22:47:17 +0900

    refactor: provider 수정

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,9 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { useState } from 'react';
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
 	const [queryClient] = useState(() => new QueryClient());
-
 	return (
 		<SessionProvider>
 			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
