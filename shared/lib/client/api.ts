57daea6f336251f5cb9b50b534854767ee515e41
OseungKwon <00osos@naver.com>
2025-06-30 19:38:15 +0900

    refactor: axios에서 사용하던 토큰 재발급 로직 제거

--- shared/lib/client/api.ts
+++ shared/lib/client/api.ts
@@ -14,33 +14,4 @@ clientApi.interceptors.request.use(async (config) => {
 	return config;
 });
 
-clientApi.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		const originalRequest = error.config;
-		const status = error.response?.status;
-
-		if (status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true;
-
-			const session = await getSession();
-			if (!session?.refreshToken) {
-				await signOut({ callbackUrl: '/login' });
-				return Promise.reject(error);
-			}
-
-			const refreshResponse = await clientApi.post('/auth/refresh', { refreshToken: session.refreshToken });
-			if (refreshResponse.status === 200) {
-				const data = refreshResponse.data;
-				axios.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`;
-				return clientApi(originalRequest);
-			} else {
-				await signOut({ callbackUrl: '/login' });
-			}
-		}
-
-		return Promise.reject(error);
-	},
-);
-
 export default clientApi;
--- shared/lib/server/api.ts
+++ shared/lib/server/api.ts
@@ -10,45 +10,11 @@ export const serverApi = axios.create({
 
 serverApi.interceptors.request.use(async (config) => {
 	const session = await getServerSession(createNextAuthOptions(authRepository));
+	console.log('session', session);
 	if (session?.accessToken) {
 		config.headers.Authorization = `Bearer ${session.accessToken}`;
 	}
 	return config;
 });
-
-serverApi.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		const originalRequest = error.config;
-		const status = error.response?.status;
-
-		if (originalRequest.url?.includes('/auth/refresh')) {
-			return Promise.reject(error);
-		}
-
-		if (status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true;
-
-			const session = await getServerSession(createNextAuthOptions(authRepository));
-			console.log('session', session);
-			if (!session?.refreshToken) {
-				throw new Error('리프레시 토큰이 없습니다. 다시 로그인 해주세요.');
-			}
-
-			const refreshResponse = await serverApi.post('/auth/refresh', { refreshToken: session.refreshToken });
-			if (refreshResponse.status === 200) {
-				const data = refreshResponse.data;
-				const tokenUpdateError = new Error('TOKEN_REFRESHED');
-				(tokenUpdateError as any).accessToken = data.accessToken;
-				(tokenUpdateError as any).refreshToken = data.refreshToken;
-				throw tokenUpdateError;
-			} else {
-				throw new Error('토큰 갱신에 실패했습니다. 다시 로그인 해주세요.');
-			}
-		}
-
-		return Promise.reject(error);
-	},
-);
 
 export default serverApi;
