fcdf87d5a1654ca5a50adb002c94242debdb8bba
OseungKwon <00osos@naver.com>
2025-07-02 17:17:04 +0900

    style: 전체 코드 포멧팅

--- features/article/hooks/use-open-in-new-window.ts
+++ features/article/hooks/use-open-in-new-window.ts
@@ -1,9 +1,9 @@
 import { useCallback } from 'react';
 
 export function useOpenInNewWindow(url: string | undefined | null) {
-  return useCallback(() => {
-    if (url) {
-      window.open(url, '_blank', 'noopener,noreferrer');
-    }
-  }, [url]);
+	return useCallback(() => {
+		if (url) {
+			window.open(url, '_blank', 'noopener,noreferrer');
+		}
+	}, [url]);
 }
