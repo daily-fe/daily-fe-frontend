eb3e35724a75dad585f9675a30ce01e8539ea932
OseungKwon <00osos@naver.com>
2025-07-04 01:07:25 +0900

    feat: icon button 공통 컴포넌트 추가

---
+++ shared/ui/IconButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { cn } from '../lib/utils';
+import { Button, ButtonProps } from './button';
+import { Icon } from './Icon';
+
+interface IconButtonProps extends ButtonProps {
+	icon: string;
+}
+
+const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(({ icon, className, ...props }, ref) => {
+	return (
+		<Button
+			variant="ghost"
+			size="icon"
+			className={cn('flex items-center justify-center', className)}
+			ref={ref}
+			{...props}
+		>
+			<Icon name={icon} className="w-full h-full" />
+		</Button>
+	);
+});
+
+export default IconButton;
