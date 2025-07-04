8c26dd59481f85620d293d603df6dd79f381538e
OseungKwon <00osos@naver.com>
2025-07-04 18:15:15 +0900

    feat: 네이게이션 바 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
+import React from 'react';
 import { Toaster } from 'sonner';
 import Header from '@/widgets/header/ui/Header';
-import './globals.css';
 import SidebarPanel from '@/widgets/sidebar/ui/SidebarPanel';
+import './globals.css';
+import { navigationMenuLinks } from '@/shared/constants/navigationMenuLinks.constant';
 import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -30,7 +33,21 @@ export default function RootLayout({
 					<SidebarPanel />
 					<div className="flex flex-col min-h-screen">
 						<Header />
-						{children}
+						<div className="flex flex-col w-full">
+							<div className="hidden sm:flex w-full border-b border-gray-200 container mx-auto">
+								<ul className="flex gap-2 items-center">
+									{navigationMenuLinks.map((item, idx) => (
+										<React.Fragment key={item.href}>
+											{idx > 0 && <div className="w-px h-4 bg-gray-300"></div>}
+											<Link href={item.href} className="p-2 text-gray-500 hover:text-gray-700">
+												{item.text}
+											</Link>
+										</React.Fragment>
+									))}
+								</ul>
+							</div>
+							<div className="flex-1 overflow-y-auto p-4">{children}</div>
+						</div>
 					</div>
 				</Providers>
 				<Toaster />
