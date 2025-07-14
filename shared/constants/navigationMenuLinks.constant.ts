6e0a73ff79a88e3db01d29d3a25f4e3b0a75c6de
OseungKwon <00osos@naver.com>
2025-07-14 15:05:45 +0900

    feat: navigation 에 feed 추가

--- shared/constants/navigationMenuLinks.constant.ts
+++ shared/constants/navigationMenuLinks.constant.ts
@@ -2,4 +2,5 @@ export const navigationMenuLinks = [
 	{ href: '/', text: '홈' },
 	{ href: '/profile', text: '프로필' },
 	{ href: '/article/liked', text: '좋아요한 글' },
+	{ href: '/feed', text: '기술 블로그 피드' },
 ];
