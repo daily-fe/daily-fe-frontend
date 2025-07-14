94b31ec529641e01fcbf3adb100fbb93d8ecaae2
OseungKwon <00osos@naver.com>
2025-07-14 15:29:42 +0900

    design: feed card ui 수정

--- features/feed/ui/FeedCard.tsx
+++ features/feed/ui/FeedCard.tsx
@@ -6,7 +6,7 @@ import { Card } from '@/shared/ui/card';
 
 export function FeedCard({ article }: { article: Feed }) {
 	return (
-		<Card className="flex flex-col gap-2 p-4">
+		<Card className="flex flex-col justify-between gap-2 p-4">
 			<Link
 				href={article.url}
 				target="_blank"
@@ -15,7 +15,7 @@ export function FeedCard({ article }: { article: Feed }) {
 			>
 				{article.title}
 			</Link>
-			<div className="flex items-center gap-2 text-sm text-gray-500">
+			<div className="flex items-center justify-between gap-2 text-sm text-gray-500">
 				<Badge>{article.site}</Badge>
 				<span>{formatDateToKorean(article.publishedAt)}</span>
 			</div>
