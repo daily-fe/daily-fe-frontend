2b5f57b3cf6cc7c424f1290e59b3d207aca8a938
OseungKwon <00osos@naver.com>
2025-07-15 17:46:42 +0900

    feat: query provider 추가

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import React from 'react';
 import { SidebarProvider } from '@/shared/ui/sidebar';
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<SessionProvider>
-			<SidebarProvider>{children}</SidebarProvider>
-		</SessionProvider>
+		<QueryClientProvider client={queryClient}>
+			<SessionProvider>
+				<SidebarProvider>{children}</SidebarProvider>
+			</SessionProvider>
+		</QueryClientProvider>
 	);
 }
