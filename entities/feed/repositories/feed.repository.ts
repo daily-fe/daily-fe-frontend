200a98af10f577549e418a782560954813140656
OseungKwon <00osos@naver.com>
2025-07-15 18:22:27 +0900

    feat: server, client 호출 repository 분리

--- entities/feed/repositories/feed.repository.ts
+++ entities/feed/repositories/feed.repository.ts
@@ -1,16 +1,42 @@
 import { Feed } from '@/entities/feed/model/types';
 import { apiCall } from '@/shared/lib/api-call';
+import clientApi from '@/shared/lib/client/api';
+import { CursorPaginationResponseDto } from '@/shared/lib/dto/cursor-pagination.dto';
 import { serverApi } from '@/shared/lib/server/api';
 
 export abstract class FeedRepositoryImpl {
-	abstract getAll(): Promise<Feed[]>;
+	abstract getAll(cursor?: string | null, limit?: number): Promise<CursorPaginationResponseDto<Feed>>;
 }
 
-class FeedRepository implements FeedRepositoryImpl {
-	async getAll(): Promise<Feed[]> {
-		const response = await apiCall(serverApi.get('/feeds'), '피드 불러오기 실패');
+class FeedRepositoryWithServer implements FeedRepositoryImpl {
+	async getAll(cursor: string | null = null, limit: number = 10): Promise<CursorPaginationResponseDto<Feed>> {
+		const response = await apiCall(
+			serverApi.get('/feeds', {
+				params: {
+					cursor,
+					limit,
+				},
+			}),
+			'피드 불러오기 실패',
+		);
 		return response.data;
 	}
 }
 
-export const feedRepository = new FeedRepository();
+class FeedRepositoryWithClient implements FeedRepositoryImpl {
+	async getAll(cursor: string | null = null, limit: number = 10): Promise<CursorPaginationResponseDto<Feed>> {
+		const response = await apiCall(
+			clientApi.get('/feeds', {
+				params: {
+					cursor,
+					limit,
+				},
+			}),
+			'피드 불러오기 실패',
+		);
+		return response.data;
+	}
+}
+
+export const feedRepositoryWithServer = new FeedRepositoryWithServer();
+export const feedRepositoryWithClient = new FeedRepositoryWithClient();
