2215148db8b78cb5d3246944a643087ed9bbd155
OseungKwon <00osos@naver.com>
2025-07-17 01:33:16 +0900

    feat: preview hover 범위 수정

--- features/feed/ui/FeedPreviewCard.tsx
+++ features/feed/ui/FeedPreviewCard.tsx
@@ -6,19 +6,14 @@ import { Card } from '@/shared/ui/card';
 
 export function FeedPreviewCard({ article }: { article: Feed }) {
 	return (
-		<Card className="flex flex-col justify-between gap-2 p-4 h-full">
-			<Link
-				href={article.url}
-				target="_blank"
-				rel="noopener noreferrer"
-				className="font-semibold text-base hover:underline line-clamp-2"
-			>
-				{article.title}
-			</Link>
-			<div className="flex items-center justify-between gap-2 text-sm text-gray-500">
-				<Badge>{article?.site ?? 'Others'}</Badge>
-				<span>{formatDateToKorean(article.publishedAt)}</span>
-			</div>
-		</Card>
+		<Link href={article.url} target="_blank" rel="noopener noreferrer" className="w-full h-full group">
+			<Card className="flex flex-col justify-between gap-2 p-4 h-full">
+				<span className="font-semibold text-base group-hover:underline line-clamp-2">{article.title}</span>
+				<div className="flex items-center justify-between gap-2 text-sm text-gray-500">
+					<Badge>{article?.site ?? 'Others'}</Badge>
+					<span>{formatDateToKorean(article.publishedAt)}</span>
+				</div>
+			</Card>
+		</Link>
 	);
 }
