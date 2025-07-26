7813905afab0e3310cdb635266098047bf8dbe0c
OseungKwon <00osos@naver.com>
2025-07-26 21:54:54 +0900

    chore: 배포 설정

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
+	// NextAuth 설정 확인
+	env: {
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+		API_HOST: process.env.API_HOST,
+		GITHUB_ID: process.env.GITHUB_ID,
+		GITHUB_SECRET: process.env.GITHUB_SECRET,
+	},
 };
 
 export default nextConfig;
